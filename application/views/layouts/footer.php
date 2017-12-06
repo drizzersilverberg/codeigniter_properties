c7d21caad29c92557737b0deee013b25f14eb178
Dendi Handian <dendihandian@gmail.com>
2017-12-06 07:09:02 +0000

    add layouts views

---
+++ application/views/layouts/footer.php
@@ -0,0 +1,18 @@
+        <hr>
+        <footer class="callout large secondary">
+          <div class="row">
+            <div class="large-12 columns">
+              <h5>Loading Views</h5>
+              <p>examples by: <a href="https://twitter.com/bpineda">@bpineda</a></p>
+              </h4>
+            </div>
+          </div>
+        </footer>
+        <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/js/foundation.min.js"></script>
+        <script>
+          $(document).foundation();
+        </script>
+
+    </body>
+</html>

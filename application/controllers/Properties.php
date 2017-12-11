b4e6cb324a5d23502ca84b9412aa93348716a80b
Dendi Handian <dendihandian@gmail.com>
2017-12-11 12:31:47 +0700

    add show() action method in Properties controller

--- application/controllers/Properties.php
+++ application/controllers/Properties.php
@@ -2,6 +2,10 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 class Properties extends CI_Controller
 {
+    /**
+     * Show Properties List
+     * [GET] BASE_URL/properties/index
+     */
     public function index()
     {
         $this->load->helpers('url');
@@ -11,5 +15,17 @@ class Properties extends CI_Controller
         $this->load->view('layouts/foundation_nav');
         $this->load->view('properties/index', $data);
         $this->load->view('layouts/footer');
+    }
+
+    /**
+     * Show A Property Detail
+     * [GET] homestead.codeigniter.dev/properties/show/{id}
+     *
+     * @param      integer  $id     Property identifier
+     */
+    public function show($id)
+    {
+        $data['id'] = $id;
+        $this->load->view('properties/show', $data);
     }
 }

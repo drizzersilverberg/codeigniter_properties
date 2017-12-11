1ac6a9a57c08d3b1b0d0cd5319bde3871f1ad71b
Dendi Handian <dendihandian@gmail.com>
2017-12-11 14:54:44 +0700

    add properties/edit view

---
+++ application/views/properties/edit.php
@@ -0,0 +1,19 @@
+<div>
+    <div class="row column text-center">
+
+    </div>
+</div>
+<hr>
+
+<div class="row column">
+    <h3>Edit details</h3>
+    <div class="row column text-left">
+        <form method="POST">
+            <label>Name</label>
+            <input type="text" name="name" value="">
+            <label>Description</label>
+            <textarea name="description"></textarea>
+            <input class="button success" type="submit" name="" value="SAVE">
+        </form>
+    </div>
+</div>

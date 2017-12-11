e16d6736e87d63071c2359513e8791f09dc71828
Dendi Handian <dendihandian@gmail.com>
2017-12-11 15:15:02 +0700

    use property data as default value in properties/edit view

--- application/views/properties/edit.php
+++ application/views/properties/edit.php
@@ -10,9 +10,9 @@
     <div class="row column text-left">
         <form method="POST">
             <label>Name</label>
-            <input type="text" name="name" value="">
+            <input type="text" name="name" value="<?php echo $property['name']; ?>">
             <label>Description</label>
-            <textarea name="description"></textarea>
+            <textarea name="description"><?php echo $property['description']; ?></textarea>
             <input class="button success" type="submit" name="" value="SAVE">
         </form>
     </div>

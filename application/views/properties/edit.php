065ce9f7b6cf008e540994a8e4660b410620019f
Dendi Handian <dendihandian@gmail.com>
2017-12-12 08:42:12 +0700

    add file input with form_open_multipart('')

--- application/views/properties/edit.php
+++ application/views/properties/edit.php
@@ -9,10 +9,12 @@
     <h3>Edit details</h3>
     <div class="row column text-left">
         <form method="POST">
+        <?php  echo form_open_multipart(''); ?>
             <label>Name</label>
             <input type="text" name="name" value="<?php echo $property['name']; ?>">
             <label>Description</label>
             <textarea name="description"><?php echo $property['description']; ?></textarea>
+            <input type="file" name="image_file">
             <input class="button success" type="submit" name="" value="SAVE">
         </form>
     </div>
